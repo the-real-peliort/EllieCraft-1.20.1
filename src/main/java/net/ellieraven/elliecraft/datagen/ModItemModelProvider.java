b4f64871f5def0053d6ca08a158b02d470683b84
Ellie Raven <106265080+the-real-peliort@users.noreply.github.com>
2026-04-11 18:19:05 +0200

    The rest of the thigh high stuff

--- src/main/java/net/ellieraven/elliecraft/datagen/ModItemModelProvider.java
+++ src/main/java/net/ellieraven/elliecraft/datagen/ModItemModelProvider.java
@@ -31,6 +31,7 @@ public class ModItemModelProvider extends ItemModelProvider {
         simpleItem(ModItems.PAINT_SPONGE);
         simpleItem(ModItems.SOAP_BAR);
         simpleItem(ModItems.FATE_DELAYING_WAND);
+        simpleItem(ModItems.GIRL_THIGH_HIGHS);
 
         simpleVanillaTextureItem(ModItems.LAG_STICK, "stick");
     }
