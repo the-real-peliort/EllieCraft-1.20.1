63b75880654675be7fab61d04da34c829d7b587c
Ellie Raven <106265080+the-real-peliort@users.noreply.github.com>
2026-04-11 16:43:52 +0200

    ModArmorMaterial.java

---
+++ src/main/java/net/ellieraven/elliecraft/item/custom/ModArmorMaterials.java
@@ -0,0 +1,59 @@
+package net.ellieraven.elliecraft.item.custom;
+
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+
+    GIRL_CLOTHES;
+
+    private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
+
+    @Override
+    public int getDurabilityForType(ArmorItem.Type type) {
+        return BASE_DURABILITY[type.getSlot().getIndex()] * 25;
+    }
+
+    @Override
+    public int getDefenseForType(ArmorItem.Type type) {
+        return switch (type) {
+            case BOOTS -> 1;
+            case LEGGINGS -> 3;
+            case CHESTPLATE -> 4;
+            case HELMET -> 2;
+        };
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return 5;
+    }
+
+    @Override
+    public net.minecraft.sounds.SoundEvent getEquipSound() {
+        return SoundEvents.WOOL_HIT;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.of(Items.WHITE_WOOL);
+    }
+
+    @Override
+    public String getName() {
+        return "elliecraft:girl_clothes";
+    }
+
+    @Override
+    public float getToughness() {
+        return 1.0F;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0.0F;
+    }
+}
